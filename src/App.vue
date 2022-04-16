d0800252d2df8116243f370aa3569e9ac42bc5d8
dalychoi <dalychoi@gmail.com>
2022-04-16 20:05:03 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <main>
+    <p>Hi, please enter your name:</p>
+    <p>First: <input v-model="firstName" /></p>
+    <p>Last: <input v-model="lastName" /></p>
+    <p>{{ fullName }}</p>
+    <p><button v-on:click="reset">Reset</button></p>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: '',
+      lastName: ''
+    }
+  },
+  computed: {
+    fullName() {
+      return this.firstName + ' ' + this.lastName
+    }
+  },
+  methods: {
+    reset() {
+      this.firstName = ''
+      this.lastName = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+main {
+  font-family: Arial, Helvetica, sans-serif;
+  padding: 1em;
+  max-width: 240px;
+  margin: 0 auto;
+  background-color:#eee;
+}
+</style>
